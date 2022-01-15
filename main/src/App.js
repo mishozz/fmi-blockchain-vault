22deeef150e7692323e7be282ce619d6a3d0f767
I538212 <mihael.zlatev@sap.com>
2022-01-15 23:26:20 +0200

    set up the React project

--- main/src/App.js
+++ main/src/App.js
@@ -1,11 +1,40 @@
+import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import Web3 from 'web3'
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: ''
+    }
+  }
 
-    </div>
-  );
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts() 
+    this.setState({account: accounts[0]})
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      await window.ethereum.send('eth_requestAccounts');
+      window.web3 = new Web3(window.ethereum);
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+   
+      </div>
+    );
+  }
 }
-
 export default App;
