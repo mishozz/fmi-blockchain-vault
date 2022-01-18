e5509735ed8a56386af7b8b835ee5a9b4f8b960f
I538212 <mihael.zlatev@sap.com>
2022-01-18 23:59:08 +0200

    Add deposit functionality

--- main/src/App.js
+++ main/src/App.js
@@ -5,13 +5,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import MyNavBar from  './Components/Navbar'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Deposit from  './Components/Deposit'
+import Vault from './abis/Vault.json'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       account: '',
-      balance: '0'
+      balance: '0',
+      vault: {},
+      vaultBalance: '0'
     }
   }
 
@@ -26,6 +29,18 @@ class App extends Component {
     this.setState({account: accounts[0]})
     const balance = await web3.eth.getBalance(this.state.account)
     this.setState({balance})
+
+    // load vault contract
+    const networkId =  await web3.eth.net.getId()
+    const vaultData = Vault.networks[networkId]
+    if(vaultData) {
+      const vault = new web3.eth.Contract(Vault.abi, vaultData.address)
+      this.setState({ vault })
+      let vaultBalance = await web3.eth.getBalance(vaultData.address)
+      this.setState({vaultBalance})
+    } else {
+      window.alert('Vault contract not deployed to detected network.')
+    }
   }
 
   async loadWeb3() {
@@ -42,7 +57,7 @@ class App extends Component {
         <Router>
           <Routes>
             <Route exact path="/" />
-            <Route exact path="/deposit" element={<Deposit  balance={this.state.balance}/>}/>
+            <Route exact path="/deposit" element={<Deposit  balance={this.state.balance} vaultBalance={this.state.vaultBalance} vault={this.state.vault} account={this.state.account}/>}/>
           </Routes>
       </Router>
       </div>
--- main/src/Components/Deposit.js
+++ main/src/Components/Deposit.js
@@ -1,31 +1,77 @@
 import { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {InputGroup, Button, FormControl} from 'react-bootstrap'
-import Web3 from 'web3'
+import ReactDOM from 'react-dom'
 
 
 class Deposit extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+          loading: true
+        }
+    }
+
+    async componentDidMount() {
+        await new Promise(r => setTimeout(r, 100));
+        this.setState({loading: false})
+    }
+
     getBalance = () => {
-        return "Balance is " + this.props.balance + " Wei"
+        return "Your Balance is " + window.web3.utils.fromWei(this.props.balance, 'Ether') + " ETH"
     }
 
 
+    getVaultBalance = () => {
+        return "Vault Balance is " + window.web3.utils.fromWei(this.props.vaultBalance, 'Ether') + " ETH"
+    }
 
-  render() {
-    return (
-      <div style={{
-        position: 'absolute', left: '50%', top: '50%',
-        transform: 'translate(-50%, -50%)'
-    }}>
-        <h1>Deposit</h1>
-        <h2 id="balance-h2">{this.getBalance()}</h2>
-        <InputGroup className="mb-3" >
-            <Button variant="outline-secondary" id="deposit-btn">Deposit</Button>
-        <FormControl
-             aria-describedby="basic-addon1"
-        />
-        </InputGroup>
-      </div>
+    deposit = (etherAmount) => {
+        this.setState({ loading: true })
+        console.log(this.props.balance)
+        console.log(this.props.account)
+        console.log(etherAmount)
+        this.props.vault.methods.deposit()
+            .send({from: this.props.account, value: etherAmount})
+            .on('transactionHash', (hash) => {
+          this.setState({ loading: false })
+        })
+        this.setState({ loading: false })
+      }
+
+    render() {
+        let content
+        if(this.state.loading) {
+        content = <p id="loader" className="text-center">Loading...</p>
+        } else {
+        content = <div>
+            <h1>Deposit</h1>
+            <h2 id="balance-h2">{this.getBalance()}</h2>
+            <h2 id="vaultBalance-h2">{this.getVaultBalance()}</h2>
+            <form onSubmit={(event) => {
+                event.preventDefault();
+                let etherAmount = this.input.value.toString()
+                etherAmount = window.web3.utils.toWei(etherAmount, 'Ether')
+                this.deposit(etherAmount)
+            }}>
+                <InputGroup className="mb-3" >
+                    <Button variant="outline-secondary" id="deposit-btn" type="submit">Deposit</Button>
+                    <FormControl 
+                        ref={(input) => this.input = input} 
+                        aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
+            </form>
+        </div>
+        }
+        return (
+        <div style={{
+            position: 'absolute', left: '50%', top: '50%',
+            transform: 'translate(-50%, -50%)'}
+        }>
+            {content}
+        </div>
     );
   }
 }
